b2b784e1d4fe1edb94d5d9e5dfac585e24bb8c51
guduruMokshitha <123493000+guduruMokshitha@users.noreply.github.com>
2023-06-03 14:56:14 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,18 @@
+import datetime
+import time
+import winsound
+
+def alarm(set_alarm_time):
+    while True:
+        time.sleep(1)
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        if current_time == set_alarm_time:
+            print("Time to Wake up")
+            for i in range(5):
+                winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
+            break
+
+print("Set alarm in 24 hour HH:MM:SS format")
+alarm_time=input("Enter the alarm time: ")
+
+alarm(alarm_time)
